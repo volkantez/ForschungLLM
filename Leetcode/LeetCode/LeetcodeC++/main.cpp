d8879fbe9e91a42a058bd77920f2a2b7371f8ee7
volkantez <117591416+volkantez@users.noreply.github.com>
2025-02-08 14:44:18 +0100

    Add files via upload

---
+++ Leetcode/LeetCode/LeetcodeC++/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "LeetCode C++ Test!" << endl;
+    return 0;
+}
---
+++ Leetcode/LeetCode/LeetcodePython/test_two_sum.py
@@ -0,0 +1,7 @@
+def two_sum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in seen:
+            return [seen[diff], i]
+        seen[num] = i
